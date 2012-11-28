c9cc51a52df913e34f559540fca99f82e954e7ea
Ayende Rahien <ayende@ayende.com>
2012-11-28 11:53:29 +0000

    fun

--- Southsand/Model/Customer.cs
+++ Southsand/Model/Customer.cs
@@ -33,7 +33,7 @@ namespace Southsand.Model
 
 		public object NullSafeGet(IDataReader rs, string[] names, object owner)
 		{
-			var result = (string) NHibernateUtil.String.NullSafeGet(rs, names);
+			var result = (string)NHibernateUtil.String.NullSafeGet(rs, names);
 			if (result == null)
 				return null;
 			return JsonConvert.DeserializeObject<Dictionary<string, string>>(result);
@@ -48,7 +48,7 @@ namespace Southsand.Model
 
 		public object DeepCopy(object value)
 		{
-			return new Dictionary<string, string>((IDictionary<string, string>) value);
+			return new Dictionary<string, string>((IDictionary<string, string>)value);
 		}
 
 		public object Replace(object original, object target, object owner)
@@ -68,12 +68,101 @@ namespace Southsand.Model
 
 		public SqlType[] SqlTypes
 		{
-			get { return new[] {NHibernateUtil.String.SqlType}; }
+			get { return new[] { NHibernateUtil.String.SqlType }; }
 		}
 
 		public Type ReturnedType
 		{
-			get { return typeof (IDictionary<string, string>); }
+			get { return typeof(IDictionary<string, string>); }
+		}
+
+		public bool IsMutable
+		{
+			get { return true; }
+		}
+	}
+
+	public class NastyBooleanType : IUserType
+	{
+		public bool Equals(object x, object y)
+		{
+			return object.Equals(x, y);
+		}
+
+		public int GetHashCode(object x)
+		{
+			return x.GetHashCode();
+		}
+
+		public object NullSafeGet(IDataReader rs, string[] names, object owner)
+		{
+			var val = rs[names[0]];
+
+			if (val == DBNull.Value)
+				return false;
+
+			if (val is int)
+				return ((int)val) != 0;
+
+			var s = val as string;
+			if (s != null)
+			{
+				if ("false".Equals(s, StringComparison.InvariantCulture) ||
+					"null".Equals(s, StringComparison.InvariantCulture) ||
+					"no".Equals(s, StringComparison.InvariantCulture) ||
+					"0".Equals(s, StringComparison.InvariantCulture) ||
+					"n".Equals(s, StringComparison.InvariantCulture))
+					return false;
+			}
+
+			if (val is bool)
+			{
+				return val;
+			}
+
+			return true;
+		}
+
+		public void NullSafeSet(IDbCommand cmd, object value, int index)
+		{
+			if (value == null)
+			{
+				((IDataParameter)cmd.Parameters[index]).Value = DBNull.Value;
+			}
+			else
+			{
+				((IDataParameter)cmd.Parameters[index]).Value = (bool)value;
+			}
+		}
+
+		public object DeepCopy(object value)
+		{
+			return new Dictionary<string, string>((IDictionary<string, string>)value);
+		}
+
+		public object Replace(object original, object target, object owner)
+		{
+			return DeepCopy(target);
+		}
+
+		public object Assemble(object cached, object owner)
+		{
+			return cached;
+		}
+
+		public object Disassemble(object value)
+		{
+			return value;
+		}
+
+		public SqlType[] SqlTypes
+		{
+			get { return new[] { NHibernateUtil.String.SqlType }; }
+		}
+
+		public Type ReturnedType
+		{
+			get { return typeof(IDictionary<string, string>); }
 		}
 
 		public bool IsMutable
