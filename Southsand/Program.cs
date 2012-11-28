9cb5ea2e55360e710d2c86485eb1b74692f6d7fc
Ayende Rahien <ayende@ayende.com>
2012-11-28 14:36:51 +0000

    Updating

--- Southsand/Program.cs
+++ Southsand/Program.cs
@@ -2,11 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq.Expressions;
+using NHibernate;
 using Southsand.Infrastructure;
 using Southsand.Model;
+using NHibernate.Linq;
+using System.Linq;
+
 
 namespace Southsand
 {
+
+	public static class NHExtensions
+	{
+		public static IFutureValue<TResult> ToFutureValue<TSource, TResult>(
+			this IQueryable<TSource> source,
+			Expression<Func<IQueryable<TSource>, TResult>> selector)
+			where TResult : struct
+		{
+
+			var provider = (INhQueryProvider) source.Provider;
+			var method = ((MethodCallExpression) selector.Body).Method;
+			var expression = Expression.Call(null, method, source.Expression);
+			return (IFutureValue<TResult>) provider.ExecuteFuture(expression);
+		}
+	}
+
 	static class Program
 	{
 		/// <summary>
@@ -20,11 +41,23 @@ namespace Southsand
 			using (var session = Global.SessionFactory.OpenSession())
 			using (session.BeginTransaction())
 			{
-				var customer = session.Get<Customer>(2L);
-				session.Transaction.Commit();
+				var list = session.Query<Customer>()
+					   .Take(5)
+					   .Skip(10)
+					   .ToFuture();
+
+				var cnt = session.Query<Customer>()
+				                 .ToFutureValue(x=>x.Count());
+
+
+				Console.WriteLine(cnt.Value);
+				foreach (var customer in list)
+				{
+					Console.WriteLine(customer.Email);
+				}
 			}
 
-			
+
 
 			//Application.EnableVisualStyles();
 			//Application.SetCompatibleTextRenderingDefault(false);
